5c087450a9f7f384adc6ca3109912857b07d7e6e
ErnestasK760 <82801762+ErnestasK760@users.noreply.github.com>
2021-10-07 14:13:41 +0300

    Created routes
    Created routes for Author and Book entities

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'authors'], function(){
+    Route::get('', [AuthorController::class, 'index'])->name('author.index');
+    Route::get('create', [AuthorController::class, 'create'])->name('author.create');
+    Route::post('store', [AuthorController::class, 'store'])->name('author.store');
+    Route::get('edit/{author}', [AuthorController::class, 'edit'])->name('author.edit');
+    Route::post('update/{author}', [AuthorController::class, 'update'])->name('author.update');
+    Route::post('delete/{author}', [AuthorController::class, 'destroy'])->name('author.destroy');
+    Route::get('show/{author}', [AuthorController::class, 'show'])->name('author.show');
+ });
+ Route::group(['prefix' => 'books'], function(){
+    Route::get('', [BookController::class, 'index'])->name('book.index');
+    Route::get('create', [BookController::class, 'create'])->name('book.create');
+    Route::post('store', [BookController::class, 'store'])->name('book.store');
+    Route::get('edit/{book}', [BookController::class, 'edit'])->name('book.edit');
+    Route::post('update/{book}', [BookController::class, 'update'])->name('book.update');
+    Route::post('delete/{book}', [BookController::class, 'destroy'])->name('book.destroy');
+    Route::get('show/{book}', [BookController::class, 'show'])->name('book.show');
+ });
 
 Auth::routes();
 
